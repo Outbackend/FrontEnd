e68b1dbecc02a9f9187a886e58eee77557d10847
jeonghyun06 <roskrlss85@gmail.com>
2024-06-25 18:50:27 +0900

    feat: 답글 기능 수정

--- src/components/Page/Project/Comment.jsx
+++ src/components/Page/Project/Comment.jsx
@@ -7,79 +7,21 @@ import userDetailStore from "../../../variables/States/UserDetailStore";
 const Comment = ({ projectId, initialComments }) => {
   const [comments, setComments] = useState(initialComments);
   const [newComment, setNewComment] = useState("");
-  const [replyComment, setReplyComment] = useState("");
-  const [parentCommentId, setParentCommentId] = useState(null);
-  const { user } = useLoginStore((state) => ({ user: state.user }));
+  const [activeReplyId, setActiveReplyId] = useState(null);
+  const [replyContent, setReplyContent] = useState("");
+  const { user } = useLoginStore();
   const { fetchData } = userDetailStore();
   const navigate = useNavigate();
-  const nicknameCache = {}; // 캐시를 저장할 객체
+  const nicknameCache = {};
 
-  const handleCommentChange = (e) => {
-    setNewComment(e.target.value);
-  };
-
-  const handleReplyCommentChange = (e) => {
-    setReplyComment(e.target.value);
-  };
-
-  const formatDate = (datetime) => {
-    const date = new Date(datetime);
-    return date.toLocaleString();
-  };
-
-  // 댓글 추가
-  const handleAddComment = async () => {
-    if (newComment.trim() !== "") {
-      const newCommentObject = {
-        content: newComment,
-        projectId: projectId,
-        parentId: parentCommentId,
-      };
-
-      try {
-        const response = await axios.post(
-          `${process.env.REACT_APP_API_URL}/project/${projectId}/comment`,
-          newCommentObject
-        );
-        setComments([...comments, response.data]);
-        setNewComment("");
-        setParentCommentId(null);
-      } catch (error) {
-        console.error("Error adding comment:", error);
-      }
-    }
-  };
-
-  const handleAddReplyComment = async () => {
-    if (replyComment.trim() !== "") {
-      const newReplyCommentObject = {
-        content: replyComment,
-        projectId: projectId,
-        parentId: parentCommentId,
-      };
-
-      try {
-        const response = await axios.post(
-          `${process.env.REACT_APP_API_URL}/project/${projectId}/comment`,
-          newReplyCommentObject
-        );
-        setComments([...comments, response.data]);
-        setReplyComment("");
-        setParentCommentId(null);
-      } catch (error) {
-        console.error("Error adding reply comment:", error);
-      }
-    }
-  };
+  const formatDate = (datetime) => new Date(datetime).toLocaleString();
 
   const fetchNickname = async (userId) => {
-    if (nicknameCache[userId]) {
-      return nicknameCache[userId];
-    }
+    if (nicknameCache[userId]) return nicknameCache[userId];
     try {
       const response = await fetchData(userId);
       const nickname = response.userInfo.nickname;
-      nicknameCache[userId] = nickname; // 캐시에 저장
+      nicknameCache[userId] = nickname;
       return nickname;
     } catch (error) {
       console.error("Error fetching user nickname:", error);
@@ -87,20 +29,64 @@ const Comment = ({ projectId, initialComments }) => {
     }
   };
 
-  const handleReplyComment = (commentId) => {
-    setParentCommentId(commentId);
+  const handleAddComment = async () => {
+    if (newComment.trim() === "") return;
+
+    const newCommentObject = {
+      content: newComment,
+      projectId,
+      parentId: null,
+      userId: user,
+    };
+
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_API_URL}/project/${projectId}/comment`,
+        newCommentObject
+      );
+      const nickname = await fetchNickname(user);
+      setComments((comments) => [{ ...response.data, nickname }, ...comments]);
+      setNewComment("");
+    } catch (error) {
+      console.error("Error adding comment:", error);
+    }
   };
 
-  // 댓글 삭제
-  const handleDeleteComment = async (commentId) => {
+  const handleAddReplyComment = async (cId) => {
+    if (replyContent.trim() === "") return;
+
+    const newReplyCommentObject = {
+      content: replyContent,
+      projectId,
+      parentId: cId,
+      userId: user,
+    };
+
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_API_URL}/project/${projectId}/comment`,
+        newReplyCommentObject
+      );
+      const nickname = await fetchNickname(user);
+      setComments((comments) => [{ ...response.data, nickname }, ...comments]);
+      setReplyContent("");
+      setActiveReplyId(null);
+    } catch (error) {
+      console.error("Error adding reply comment:", error);
+    }
+  };
+
+  const handleDeleteComment = async (cId) => {
     try {
       await axios.delete(
         `${process.env.REACT_APP_API_URL}/project/${projectId}/comment`,
-        {
-          data: { commentId: commentId },
-        }
+        { data: { commentId: cId } }
       );
-      setComments(comments.filter((comment) => comment.id !== commentId));
+      setComments((comments) =>
+        comments.filter(
+          (comment) => comment.id !== cId && comment.parentId !== cId
+        )
+      );
     } catch (error) {
       console.error("Error deleting comment:", error);
     }
@@ -109,16 +95,120 @@ const Comment = ({ projectId, initialComments }) => {
   useEffect(() => {
     const loadNicknames = async () => {
       const updatedComments = await Promise.all(
-        comments.map(async (comment) => {
-          const nickname = await fetchNickname(comment.userId);
-          return { ...comment, nickname };
-        })
+        comments.map(async (comment) => ({
+          ...comment,
+          nickname: await fetchNickname(comment.userId),
+        }))
       );
       setComments(updatedComments);
     };
 
     loadNicknames();
-  }, [initialComments]); // initialComments가 변경될 때만 다시 로드
+  }, [comments]);
+
+  const renderComment = (comment) => (
+    <div
+      key={comment.id}
+      className="p-4 bg-white border border-gray-200 rounded-lg shadow-sm"
+    >
+      <div className="flex items-center mb-2">
+        <img
+          src="/UserDefault.jpg"
+          className="w-10 h-10 rounded-full mr-3"
+          alt="User Avatar"
+        />
+        <div
+          className="text-sm font-medium text-gray-900 cursor-pointer hover:text-blue-500 transition duration-200"
+          onClick={() => navigate(`/userinfo/${comment.userId}`)}
+        >
+          {comment.nickname || `user ${comment.userId}`}
+        </div>
+      </div>
+      <div className="ml-13 text-gray-700">
+        <div className="mb-1">{comment.content}</div>
+        <div className="text-xs text-gray-500">
+          {formatDate(comment.datetime)}
+        </div>
+      </div>
+      <div className="mt-2 space-x-2">
+        {user && (
+          <>
+            <button
+              className="text-sm text-blue-500 hover:text-blue-600 transition duration-200"
+              onClick={() =>
+                setActiveReplyId(
+                  activeReplyId === comment.id ? null : comment.id
+                )
+              }
+            >
+              답글달기
+            </button>
+            {user === comment.userId && (
+              <button
+                className="text-sm text-red-500 hover:text-red-600 transition duration-200"
+                onClick={() => handleDeleteComment(comment.id)}
+              >
+                삭제
+              </button>
+            )}
+          </>
+        )}
+      </div>
+      {activeReplyId === comment.id && (
+        <div className="mt-4 ml-8">
+          <textarea
+            className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent transition duration-200"
+            placeholder="답글을 작성해주세요."
+            value={replyContent}
+            onChange={(e) => setReplyContent(e.target.value)}
+          />
+          <button
+            className="mt-2 px-4 py-2 bg-blue-200 text-gray-700 font-semibold rounded-lg hover:bg-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50 transition duration-200"
+            onClick={() => handleAddReplyComment(comment.id)}
+          >
+            답글달기
+          </button>
+        </div>
+      )}
+      {comments
+        .filter((reply) => reply.parentId === comment.id)
+        .sort((a, b) => new Date(b.datetime) - new Date(a.datetime))
+        .map((reply) => (
+          <div
+            key={reply.id}
+            className="ml-8 mt-4 p-3 bg-gray-50 border border-gray-200 rounded-lg"
+          >
+            <div className="flex items-center mb-2">
+              <img
+                src="/UserDefault.jpg"
+                className="w-8 h-8 rounded-full mr-3"
+                alt="User Avatar"
+              />
+              <div
+                className="text-sm font-medium text-gray-900 cursor-pointer hover:text-blue-500 transition duration-200"
+                onClick={() => navigate(`/userinfo/${reply.userId}`)}
+              >
+                {reply.nickname || `user ${reply.userId}`}
+              </div>
+            </div>
+            <div className="ml-13 text-gray-700">
+              <div className="mb-1">{reply.content}</div>
+              <div className="text-xs text-gray-500">
+                {formatDate(reply.datetime)}
+              </div>
+            </div>
+            {user === reply.userId && (
+              <button
+                className="text-sm text-red-500 hover:text-red-600 transition duration-200"
+                onClick={() => handleDeleteComment(reply.id)}
+              >
+                삭제
+              </button>
+            )}
+          </div>
+        ))}
+    </div>
+  );
 
   return (
     <div className="mt-8 w-full">
@@ -129,7 +219,7 @@ const Comment = ({ projectId, initialComments }) => {
             className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent transition duration-200"
             placeholder="댓글을 작성해주세요."
             value={newComment}
-            onChange={handleCommentChange}
+            onChange={(e) => setNewComment(e.target.value)}
           />
           <button
             className="mt-2 px-4 py-2 bg-blue-200 text-gray-600 font-semibold rounded-lg hover:bg-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50 transition duration-200"
@@ -151,108 +241,10 @@ const Comment = ({ projectId, initialComments }) => {
         ) : (
           comments
             .filter((comment) => comment.parentId === null)
-            .map((c) => (
-              <div
-                key={c.id}
-                className="p-4 bg-white border border-gray-200 rounded-lg shadow-sm"
-              >
-                <div className="flex items-center mb-2">
-                  <img
-                    src="/UserDefault.jpg"
-                    className="w-10 h-10 rounded-full mr-3"
-                    alt="User Avatar"
-                  />
-                  <div
-                    className="text-sm font-medium text-gray-900 cursor-pointer hover:text-blue-500 transition duration-200"
-                    onClick={() => navigate(`/User/${c.userId}`)}
-                  >
-                    {c.nickname || `user ${c.userId}`}
-                  </div>
-                </div>
-                <div className="ml-13 text-gray-700">
-                  <div className="mb-1">{c.content}</div>
-                  <div className="text-xs text-gray-500">
-                    {formatDate(c.datetime)}
-                  </div>
-                </div>
-                <div className="mt-2 space-x-2">
-                  {user && (
-                    <div className="mt-2 space-x-2">
-                      <button
-                        className="text-sm text-blue-500 hover:text-blue-600 transition duration-200"
-                        onClick={() => handleReplyComment(c.id)}
-                      >
-                        답글달기
-                      </button>
-                      {user === c.userId && (
-                        <button
-                          className="text-sm text-red-500 hover:text-red-600 transition duration-200"
-                          onClick={() => handleDeleteComment(c.id)}
-                        >
-                          삭제
-                        </button>
-                      )}
-                    </div>
-                  )}
-                </div>
-                {comments
-                  .filter((comment) => comment.parentId === c.id)
-                  .map((reply) => (
-                    <div
-                      key={reply.id}
-                      className="ml-8 mt-4 p-3 bg-gray-50 border border-gray-200 rounded-lg"
-                    >
-                      <div className="flex items-center mb-2">
-                        <img
-                          src="/UserDefault.jpg"
-                          className="w-8 h-8 rounded-full mr-3"
-                          alt="User Avatar"
-                        />
-                        <div
-                          className="text-sm font-medium text-gray-900 cursor-pointer hover:text-blue-500 transition duration-200"
-                          onClick={() => navigate(`/User/${reply.userId}`)}
-                        >
-                          {reply.nickname || `user ${reply.userId}`}
-                        </div>
-                      </div>
-                      <div className="ml-13 text-gray-700">
-                        <div className="mb-1">{reply.content}</div>
-                        <div className="text-xs text-gray-500">
-                          {formatDate(reply.datetime)}
-                        </div>
-                      </div>
-                      <div className="mt-2 space-x-2">
-                        {user && user === reply.userId && (
-                          <button
-                            className="text-sm text-red-500 hover:text-red-600 transition duration-200"
-                            onClick={() => handleDeleteComment(reply.id)}
-                          >
-                            삭제
-                          </button>
-                        )}
-                      </div>
-                    </div>
-                  ))}
-              </div>
-            ))
+            .sort((a, b) => new Date(b.datetime) - new Date(a.datetime))
+            .map(renderComment)
         )}
       </div>
-      {user && parentCommentId && (
-        <div className="mt-4 ml-8">
-          <textarea
-            className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent transition duration-200"
-            placeholder="답글을 작성해주세요."
-            value={replyComment}
-            onChange={handleReplyCommentChange}
-          />
-          <button
-            className="mt-2 px-4 py-2 bg-blue-200 text-gray-700 font-semibold rounded-lg hover:bg-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50 transition duration-200"
-            onClick={handleAddReplyComment}
-          >
-            답글달기
-          </button>
-        </div>
-      )}
     </div>
   );
 };
