de26e0eba7c7943202d4c93c8e944820ef0b855d
jeonghyun06 <roskrlss85@gmail.com>
2024-06-27 01:41:47 +0900

    feat: 댓글 닉네임 설정

--- src/components/Page/Project/Comment.jsx
+++ src/components/Page/Project/Comment.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import useLoginStore from "../../../variables/States/LoginStore";
-import userDetailStore from "../../../variables/States/UserDetailStore";
 import AddComment from "./AddComment";
 import EditComment from "./EditComment";
 import ReplyComment from "./ReplyComment";
+import UserNickname from "./UserNickname";
 import axios from "axios";
 
 const Comment = ({ projectId, initialComments }) => {
@@ -12,36 +12,25 @@ const Comment = ({ projectId, initialComments }) => {
   const [activeReplyId, setActiveReplyId] = useState(null);
   const [editingCommentId, setEditingCommentId] = useState(null);
   const { user, token } = useLoginStore();
-  const { fetchData } = userDetailStore();
   const navigate = useNavigate();
-  const nicknameCache = {};
 
   const formatDate = (datetime) => new Date(datetime).toLocaleString();
 
-  const fetchNickname = async (userId) => {
-    if (nicknameCache[userId]) return nicknameCache[userId];
-    try {
-      const response = await fetchData(userId);
-      const nickname = response.userInfo.nickname;
-      nicknameCache[userId] = nickname;
-      return nickname;
-    } catch (error) {
-      console.error("Error fetching user nickname:", error);
-      return "Unknown";
-    }
+  const handleCommentAdded = (newComment) => {
+    setComments((prevComments) => [newComment, ...prevComments]);
   };
 
-  const handleCommentAdded = async (newComment) => {
-    const nickname = await fetchNickname(user);
-    setComments((prevComments) => [
-      { ...newComment, nickname },
-      ...prevComments,
-    ]);
-  };
-
-  const handleReplyAdded = async (newReply) => {
-    const nickname = await fetchNickname(user);
-    setComments((prevComments) => [{ ...newReply, nickname }, ...prevComments]);
+  const handleReplyAdded = (newReply) => {
+    setComments((prevComments) => {
+      const updatedComments = prevComments.reduce((acc, comment) => {
+        acc.push(comment);
+        if (comment.id === newReply.parentId) {
+          acc.push(newReply);
+        }
+        return acc;
+      }, []);
+      return updatedComments;
+    });
     setActiveReplyId(null);
   };
 
@@ -76,99 +65,103 @@ const Comment = ({ projectId, initialComments }) => {
   };
 
   useEffect(() => {
-    const loadNicknames = async () => {
-      const updatedComments = await Promise.all(
-        comments.map(async (comment) => ({
-          ...comment,
-          nickname: await fetchNickname(comment.userId),
-        }))
-      );
-      setComments(updatedComments);
+    const fetchComments = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.REACT_APP_API_URL}/project/${projectId}/comment`
+        );
+        setComments(response.data);
+      } catch (error) {
+        console.error("Error fetching comments:", error);
+      }
     };
 
-    loadNicknames();
-  }, []);
+    fetchComments();
+  }, [projectId]);
 
   const renderComment = (comment) => (
-    <div
-      key={comment.id}
-      className="p-4 bg-white border border-gray-200 rounded-lg shadow-sm"
-    >
-      <div className="flex items-center mb-2">
-        <img
-          src="/UserDefault.jpg"
-          className="w-10 h-10 rounded-full mr-3"
-          alt="User Avatar"
-        />
-        <div
-          className="text-sm font-medium text-gray-900 cursor-pointer hover:text-blue-500 transition duration-200"
-          onClick={() => navigate(`/userinfo/${comment.userId}`)}
-        >
-          {comment.nickname || `user ${comment.userId}`}
-        </div>
-      </div>
-      <div className="ml-13 text-gray-700">
-        {editingCommentId === comment.id ? (
-          <EditComment
-            comment={comment}
-            token={token}
-            onCommentEdited={handleCommentEdited}
-            onCancelEdit={() => setEditingCommentId(null)}
+    <div key={comment.id}>
+      <div className="p-4 bg-white border border-gray-200 rounded-lg shadow-sm">
+        <div className="flex items-center mb-2">
+          <img
+            src="/UserDefault.jpg"
+            className="w-10 h-10 rounded-full mr-3"
+            alt="User Avatar"
           />
-        ) : (
-          <>
-            <div className="mb-1">{comment.content}</div>
-            <div className="text-xs text-gray-500">
-              {formatDate(comment.datetime)}
-            </div>
-          </>
-        )}
-      </div>
-      <div className="mt-2 space-x-2">
-        {user && (
-          <>
-            <button
-              className="text-sm text-blue-500 hover:text-blue-600 transition duration-200"
-              onClick={() =>
-                setActiveReplyId(
-                  activeReplyId === comment.id ? null : comment.id
-                )
-              }
-            >
-              답글달기
-            </button>
-            {user === comment.userId && (
-              <>
-                <button
-                  className="text-sm text-yellow-500 hover:text-yellow-600 transition duration-200"
-                  onClick={() => setEditingCommentId(comment.id)}
-                >
-                  수정
-                </button>
-                <button
-                  className="text-sm text-red-500 hover:text-red-600 transition duration-200"
-                  onClick={() => handleDeleteComment(comment.id)}
-                >
-                  삭제
-                </button>
-              </>
-            )}
-          </>
-        )}
+          <div
+            className="text-sm font-medium text-gray-900 cursor-pointer hover:text-blue-500 transition duration-200"
+            onClick={() => navigate(`/userinfo/${comment.userId}`)}
+          >
+            <UserNickname userId={comment.userId} />
+          </div>
+        </div>
+        <div className="ml-13 text-gray-700">
+          {editingCommentId === comment.id ? (
+            <EditComment
+              comment={comment}
+              token={token}
+              onCommentEdited={handleCommentEdited}
+              onCancelEdit={() => setEditingCommentId(null)}
+            />
+          ) : (
+            <>
+              <div className="mb-1">{comment.content}</div>
+              <div className="text-xs text-gray-500">
+                {comment.datetime !== "2030-12-31 23:59:59"
+                  ? formatDate(comment.datetime)
+                  : "방금 등록되었습니다."}
+              </div>
+            </>
+          )}
+        </div>
+        <div className="mt-2 space-x-2">
+          {user && (
+            <>
+              <button
+                className="text-sm text-blue-500 hover:text-blue-600 transition duration-200"
+                onClick={() =>
+                  setActiveReplyId(
+                    activeReplyId === comment.id ? null : comment.id
+                  )
+                }
+              >
+                답글달기
+              </button>
+              {user === comment.userId && (
+                <>
+                  <button
+                    className="text-sm text-yellow-500 hover:text-yellow-600 transition duration-200"
+                    onClick={() => setEditingCommentId(comment.id)}
+                  >
+                    수정
+                  </button>
+                  <button
+                    className="text-sm text-red-500 hover:text-red-600 transition duration-200"
+                    onClick={() => handleDeleteComment(comment.id)}
+                  >
+                    삭제
+                  </button>
+                </>
+              )}
+            </>
+          )}
+        </div>
       </div>
       {activeReplyId === comment.id && (
         <ReplyComment
           parentId={comment.id}
-          projectId={projectId}
+          projectId={Number(projectId)}
           user={user}
           token={token}
           onReplyAdded={handleReplyAdded}
         />
       )}
-      {comments
-        .filter((reply) => reply.parentId === comment.id)
-        .sort((a, b) => new Date(b.datetime) - new Date(a.datetime))
-        .map(renderComment)}
+      <div className="mt-4 ml-8">
+        {comments
+          .filter((reply) => reply.parentId === comment.id)
+          .sort((a, b) => new Date(b.datetime) - new Date(a.datetime))
+          .map(renderComment)}
+      </div>
     </div>
   );
 
@@ -194,7 +187,7 @@ const Comment = ({ projectId, initialComments }) => {
           </div>
         ) : (
           comments
-            .filter((comment) => comment.parentId === null)
+            .filter((comment) => comment.parentId === Number(projectId))
             .sort((a, b) => new Date(b.datetime) - new Date(a.datetime))
             .map(renderComment)
         )}
--- src/variables/States/UserDetailStore.jsx
+++ src/variables/States/UserDetailStore.jsx
@@ -2,106 +2,117 @@ import { create } from "zustand";
 import axios from "axios";
 
 const userDetailStore = create((set, get) => ({
-    userInfo: null,
-    loading: false,
-    error: null,
+  userInfo: null,
+  loading: false,
+  error: null,
 
-    fetchData: async (id) => {
-      set({ loading: true, error: null });
-      try {
-        const response = await axios.get(process.env.REACT_APP_API_URL + '/user/' + id);
-        set({
-          userInfo: {
-            nickname : response.data['nickname'],
-            note : response.data['note'],
-            description : response.data['description'],
-            rangeList : response.data['range'] !== undefined ? response.data['range'] : [],
-            positionList : response.data['position'] !== undefined ? response.data['position'] : [],
-            stackList : response.data['stack'] !== undefined ? response.data['stack'] : [],
-            projectLog : response.data['projectLog'] !==  undefined ? response.data['projectLog'] : []
-          },
-          loading: false 
-        });
-      } catch (error) {
-        set({ error: error.message, loading: false });
-      }
-    },
-
-    updateItem: (key, value) =>
-      set((state) => ({
+  fetchData: async (id) => {
+    set({ loading: true, error: null });
+    try {
+      const response = await axios.get(
+        process.env.REACT_APP_API_URL + "/user/" + id
+      );
+      set({
         userInfo: {
-          ...state.userInfo,
-          [key] : value,
+          nickname: response.data["nickname"],
+          note: response.data["note"],
+          description: response.data["description"],
+          rangeList:
+            response.data["range"] !== undefined ? response.data["range"] : [],
+          positionList:
+            response.data["position"] !== undefined
+              ? response.data["position"]
+              : [],
+          stackList:
+            response.data["stack"] !== undefined ? response.data["stack"] : [],
+          projectLog:
+            response.data["projectLog"] !== undefined
+              ? response.data["projectLog"]
+              : [],
         },
+        loading: false,
+      });
+      return response.data["nickname"];
+    } catch (error) {
+      set({ error: error.message, loading: false });
+    }
+  },
+
+  updateItem: (key, value) =>
+    set((state) => ({
+      userInfo: {
+        ...state.userInfo,
+        [key]: value,
+      },
     })),
 
-    updateData: async (token, id) => {
-      try {
-        const data = {
-          "nickname": get().userInfo.nickname,
-          "note": get().userInfo.note,
-          "description": get().userInfo.description,
-          "range": get().userInfo.rangeList,
-          "position": get().userInfo.positionList,
-          "stack": get().userInfo.stackList
-        }
-        const response = await axios.post(
-          process.env.REACT_APP_API_URL + '/user/' + id,
-          data,
-          { headers: { Authorization: `Bearer ${token}` } }
-        )
-      } catch (error) {
-        set({ error: error.message, loading: false });
-      }
-    },
+  updateData: async (token, id) => {
+    try {
+      const data = {
+        nickname: get().userInfo.nickname,
+        note: get().userInfo.note,
+        description: get().userInfo.description,
+        range: get().userInfo.rangeList,
+        position: get().userInfo.positionList,
+        stack: get().userInfo.stackList,
+      };
+      const response = await axios.post(
+        process.env.REACT_APP_API_URL + "/user/" + id,
+        data,
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+    } catch (error) {
+      set({ error: error.message, loading: false });
+    }
+  },
 
-    insertRangeList : (value) => 
-      set((state) => ({
-        userInfo: {
-          ...state.userInfo,
-          rangeList : [...state.userInfo.rangeList, value]
-        }
+  insertRangeList: (value) =>
+    set((state) => ({
+      userInfo: {
+        ...state.userInfo,
+        rangeList: [...state.userInfo.rangeList, value],
+      },
     })),
 
-    insertPositionList : (value) => 
-      set((state) => ({
-        userInfo: {
-          ...state.userInfo,
-          positionList : [...state.userInfo.positionList, value]
-        }
+  insertPositionList: (value) =>
+    set((state) => ({
+      userInfo: {
+        ...state.userInfo,
+        positionList: [...state.userInfo.positionList, value],
+      },
     })),
 
-    insertStackList : (value) => 
-      set((state) => ({
-        userInfo: {
-          ...state.userInfo,
-          stackList : [...state.userInfo.stackList, value]
-        }
+  insertStackList: (value) =>
+    set((state) => ({
+      userInfo: {
+        ...state.userInfo,
+        stackList: [...state.userInfo.stackList, value],
+      },
     })),
-    
-    deleteRangeList : (value) =>
-      set((state) => ({
-        userInfo: {
-          ...state.userInfo,
-          rangeList : state.userInfo.rangeList.filter((i) => i !== value)
-        }
-      })),
 
-    deletePositionList : (value) =>
-      set((state) => ({
-        userInfo: {
-          ...state.userInfo,
-          positionList : state.userInfo.positionList.filter((i) => i !== value)
-        }
-      })),
+  deleteRangeList: (value) =>
+    set((state) => ({
+      userInfo: {
+        ...state.userInfo,
+        rangeList: state.userInfo.rangeList.filter((i) => i !== value),
+      },
+    })),
 
-    deleteStackList : (value) =>
-      set((state) => ({
-        userInfo: {
-          ...state.userInfo,
-          stackList : state.userInfo.stackList.filter((i) => i !== value)
-        }
-      })),
+  deletePositionList: (value) =>
+    set((state) => ({
+      userInfo: {
+        ...state.userInfo,
+        positionList: state.userInfo.positionList.filter((i) => i !== value),
+      },
+    })),
+
+  deleteStackList: (value) =>
+    set((state) => ({
+      userInfo: {
+        ...state.userInfo,
+        stackList: state.userInfo.stackList.filter((i) => i !== value),
+      },
+    })),
 }));
 
 export default userDetailStore;
