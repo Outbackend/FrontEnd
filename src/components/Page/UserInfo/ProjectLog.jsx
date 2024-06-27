037f1f8d73ee73adf2fe2e3dc7fde80b499fe67c
uyvr97 <uyvr12211600@inha.edu>
2024-06-27 23:23:13 +0900

    fix: 프로젝트 기록마다  페이지 연결

--- src/components/Page/UserInfo/ProjectLog.jsx
+++ src/components/Page/UserInfo/ProjectLog.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
+import { useNavigate } from 'react-router-dom';
 
-const ProjectLog = ({ name, description, status, position }) => {
+const ProjectLog = ({ id, name, description, status, position }) => {
 
     const stat = status === '모집중' ? 'bg-white' : 'bg-gray-300 text-white'
     const pos = position === '' ? '' : 'bg-white shadow'
+    const navigate = useNavigate();
     return(
         <div className='relative h-[150px] border-b mt-5 [border-bottom-style:solid] border-[#dfdfdf] 
-            bg-gray-100 rounded-lg p-2 mb-2 shadow-md'>
+            bg-gray-100 rounded-lg p-2 mb-2 shadow-md'
+            onClick={()=>navigate(`/project/${id}`)}>
             <div className='relative h-[50px]'>
                 <div className='absolute h-[30px] top-[50%] mt-[-15px] left-5'>
                     <p className='text-lg font-bold'>{ name }</p>
